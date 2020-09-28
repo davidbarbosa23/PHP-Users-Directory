6b08a5da4860f99a19e88d07202e6982a80b9fd6
David Barbosa <davidbarbosa.dev@gmail.com>
2020-09-28 00:52:43 -0500

    Add Customers to migrate & trycatch by every table

--- database/migrations/TablesMigrations.php
+++ database/migrations/TablesMigrations.php
@@ -7,7 +7,7 @@ define('BASE_PATH', dirname(dirname(__DIR__)) . DS);
 require BASE_PATH . 'vendor' . DS . 'autoload.php';
 
 // Load Envirinment Variables
-$dotenv = Dotenv\Dotenv::create(BASE_PATH);
+$dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
 // Database Connection
@@ -16,29 +16,65 @@ require BASE_PATH . 'config' . DS . 'database.php';
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 // Users table
-Capsule::schema()->create(
-    'users',
-    function ($table) {
-        $table->increments('id')->unsigned();
-        $table->string('name');
-        $table->string('email')->unique();
-        $table->string('password');
-        $table->integer('country_id');
-        $table->boolean('active')->default(1);
-        $table->timestamps();
-
-        $table->index('name', 'email');
-    }
-);
+try {
+    Capsule::schema()->create(
+        'users',
+        function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->integer('country_id');
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+    
+            $table->index('name', 'email');
+        }
+    );
+    echo "Users table successfully migrated.\n\n";
+} catch (\Throwable $th) {
+    echo $th->getMessage() . "\n\n";
+}
 
 // Countries table
-Capsule::schema()->create(
-    'countries',
-    function ($table) {
-        $table->increments('id')->unsigned();
-        $table->string('name');
-        $table->boolean('active')->default(1);
-    }
-);
+try {
+    Capsule::schema()->create(
+        'countries',
+        function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->boolean('active')->default(1);
+        }
+    );
+    echo "Countries table successfully migrated.\n\n";
+} catch (\Throwable $th) {
+    echo $th->getMessage() . "\n\n";
+}
 
-echo "Migration finished successfully.\n";
+// Customers table
+try {
+    Capsule::schema()->create(
+        'customers',
+        function ($table) {
+            $table->increments('id')->unsigned();
+            $table->string('job_title');
+            $table->string('email');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('document');
+            $table->string('phone_number');
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->date('birthday')->nullable();
+            $table->string('lang')->nullable();
+    
+            $table->timestamps();
+        }
+    );
+    echo "Customers table successfully migrated.\n\n";
+} catch (\Throwable $th) {
+    echo $th->getMessage() . "\n\n";
+}
+
+echo "Migration finished.\n";
